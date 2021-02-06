609fd45d059e6bda0982c927c27d58214cfc3369
ncuyh88765 <77950331+ncuyh88765@users.noreply.github.com>
2021-02-06 23:20:47 +0800

    D-02_NUMPY_0206

---
+++ D-02_Numpy.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[19]:
+
+
+data=np.arange(30).reshape((5,6))
+data
+
+
+# In[20]:
+
+
+data=np.arange(30).reshape((5,6),order='f')
+data
+
+
+# In[23]:
+
+
+data=np.arange(30)
+a=np.where(data%6==1)
+a
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
