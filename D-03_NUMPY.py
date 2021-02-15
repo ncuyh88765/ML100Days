eaa62257b76680e5c98b2e9ce51b909bb07c58b8
ncuyh88765 <77950331+ncuyh88765@users.noreply.github.com>
2021-02-15 21:22:09 +0800

    Create D-03_NUMPY.py

---
+++ D-03_NUMPY.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import numpy as np
+a=np.add(1,3)
+print(a)
+
+
+# In[5]:
+
+
+np.sqrt(9)
+
+
+# In[6]:
+
+
+np.mod(9,4)
+
+
+# In[8]:
+
+
+a=np.array([1,2,3,4,5])
+b=np.array([2,3,4,5,6])
+a*b
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
